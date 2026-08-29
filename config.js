e837cb294cf0fd560941f4af0236f9ecd3052d4b
aipudiohelp <aipudio.help@gmail.com>
2026-08-29 21:10:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const SUPABASE_CONFIG = {
   url: "https://bimhfrxsukvfetbzksfh.supabase.co",
-  anonKey: "PUT_YOUR_SUPABASE_ANON_KEY_HERE"
+  anonKey: "sb_publishable_shqv73D6gUwiz1Q6JpMPyQ_gLevomHz"
 };
